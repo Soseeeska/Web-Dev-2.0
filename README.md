4f6faf73d44caa56dba078da11d8ca3e4001acca
Mykhaylo Porchyryane <mporchirian@gmail.com>
2024-07-02 01:23:36 +0200

    email forwarding function

---
+++ README.md
@@ -0,0 +1,2 @@
+# Web-development-project-v2.0
+# Web-development-project-v2.0
---
+++ js.js
@@ -0,0 +1,18 @@
+// Script to handle "to the top" button
+const toTopBtn = document.getElementById('toTopBtn');
+
+
+window.onscroll = function() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        toTopBtn.style.display = 'block';
+    } else {
+        toTopBtn.style.display = 'none';
+    }
+};
+
+toTopBtn.onclick = function() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+};
+
+app.use(express.static(path.join(__dirname,"public")));
